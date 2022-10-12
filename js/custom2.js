e4eb1f0d299f627f043a20317381c7bb4ee09285
mohammad-mekawy <mohammad.mekawy92@gmail.com>
2022-10-12 13:23:28 +0200

    minify

--- js/custom2.js
+++ js/custom2.js
@@ -147,292 +147,292 @@ $(document).ready(function () {
   /*	Accordion
 		/*----------------------------------------------------*/
 
-  $(".accordion > .accordion-item.is-active")
-    .children(".accordion-panel")
-    .slideDown();
+  // $(".accordion > .accordion-item.is-active")
+  //   .children(".accordion-panel")
+  //   .slideDown();
 
-  $(".accordion > .accordion-item").click(function () {
-    $(this)
-      .siblings(".accordion-item")
-      .removeClass("is-active")
-      .children(".accordion-panel")
-      .slideUp();
-    $(this)
-      .toggleClass("is-active")
-      .children(".accordion-panel")
-      .slideToggle("ease-out");
-  });
+  // $(".accordion > .accordion-item").click(function () {
+  //   $(this)
+  //     .siblings(".accordion-item")
+  //     .removeClass("is-active")
+  //     .children(".accordion-panel")
+  //     .slideUp();
+  //   $(this)
+  //     .toggleClass("is-active")
+  //     .children(".accordion-panel")
+  //     .slideToggle("ease-out");
+  // });
 
   /*----------------------------------------------------*/
   /*	Single Image Lightbox
 		/*----------------------------------------------------*/
 
-  $(".image-link").magnificPopup({
-    type: "image",
-  });
+  // $(".image-link").magnificPopup({
+  //   type: "image",
+  // });
 
   /*----------------------------------------------------*/
   /*	Video Link #1 Lightbox
 		/*----------------------------------------------------*/
 
-  $(".video-popup1").magnificPopup({
-    type: "iframe",
-    iframe: {
-      patterns: {
-        youtube: {
-          index: "youtube.com",
-          src: "https://www.youtube.com/embed/SZEflIVnhH8",
-        },
-      },
-    },
-  });
+  // $(".video-popup1").magnificPopup({
+  //   type: "iframe",
+  //   iframe: {
+  //     patterns: {
+  //       youtube: {
+  //         index: "youtube.com",
+  //         src: "https://www.youtube.com/embed/SZEflIVnhH8",
+  //       },
+  //     },
+  //   },
+  // });
 
   /*----------------------------------------------------*/
   /*	Video Link #2 Lightbox
 		/*----------------------------------------------------*/
 
-  $(".video-popup2").magnificPopup({
-    type: "iframe",
-    iframe: {
-      patterns: {
-        youtube: {
-          index: "youtube.com",
-          src: "https://www.youtube.com/embed/7e90gBu4pas",
-        },
-      },
-    },
-  });
+  // $(".video-popup2").magnificPopup({
+  //   type: "iframe",
+  //   iframe: {
+  //     patterns: {
+  //       youtube: {
+  //         index: "youtube.com",
+  //         src: "https://www.youtube.com/embed/7e90gBu4pas",
+  //       },
+  //     },
+  //   },
+  // });
 
   /*----------------------------------------------------*/
   /*	Video Link #3 Lightbox
 		/*----------------------------------------------------*/
 
-  $(".video-popup3").magnificPopup({
-    type: "iframe",
-    iframe: {
-      patterns: {
-        youtube: {
-          index: "youtube.com",
-          src: "https://www.youtube.com/embed/0gv7OC9L2s8",
-        },
-      },
-    },
-  });
+  // $(".video-popup3").magnificPopup({
+  //   type: "iframe",
+  //   iframe: {
+  //     patterns: {
+  //       youtube: {
+  //         index: "youtube.com",
+  //         src: "https://www.youtube.com/embed/0gv7OC9L2s8",
+  //       },
+  //     },
+  //   },
+  // });
 
   /*----------------------------------------------------*/
   /*	Statistic Counter
 		/*----------------------------------------------------*/
 
-  $(".count-element").each(function () {
-    $(this).appear(
-      function () {
-        $(this)
-          .prop("Counter", 0)
-          .animate(
-            {
-              Counter: $(this).text(),
-            },
-            {
-              duration: 4000,
-              easing: "swing",
-              step: function (now) {
-                $(this).text(Math.ceil(now));
-              },
-            }
-          );
-      },
-      { accX: 0, accY: 0 }
-    );
-  });
+  // $(".count-element").each(function () {
+  //   $(this).appear(
+  //     function () {
+  //       $(this)
+  //         .prop("Counter", 0)
+  //         .animate(
+  //           {
+  //             Counter: $(this).text(),
+  //           },
+  //           {
+  //             duration: 4000,
+  //             easing: "swing",
+  //             step: function (now) {
+  //               $(this).text(Math.ceil(now));
+  //             },
+  //           }
+  //         );
+  //     },
+  //     { accX: 0, accY: 0 }
+  //   );
+  // });
 
   /*----------------------------------------------------*/
   /*	Testimonials Rotator
 		/*----------------------------------------------------*/
 
-  var owl = $(".reviews-wrapper");
-  owl.owlCarousel({
-    items: 3,
-    loop: true,
-    autoplay: true,
-    navBy: 1,
-    autoplayTimeout: 4500,
-    autoplayHoverPause: true,
-    smartSpeed: 1500,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      767: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      991: {
-        items: 3,
-      },
-      1000: {
-        items: 3,
-      },
-    },
-  });
+  // var owl = $(".reviews-wrapper");
+  // owl.owlCarousel({
+  //   items: 3,
+  //   loop: true,
+  //   autoplay: true,
+  //   navBy: 1,
+  //   autoplayTimeout: 4500,
+  //   autoplayHoverPause: true,
+  //   smartSpeed: 1500,
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //     },
+  //     767: {
+  //       items: 1,
+  //     },
+  //     768: {
+  //       items: 2,
+  //     },
+  //     991: {
+  //       items: 3,
+  //     },
+  //     1000: {
+  //       items: 3,
+  //     },
+  //   },
+  // });
 
   /*----------------------------------------------------*/
   /*	Brands Logo Rotator
 		/*----------------------------------------------------*/
 
-  var owl = $(".brands-carousel");
-  owl.owlCarousel({
-    items: 5,
-    loop: true,
-    autoplay: true,
-    navBy: 1,
-    nav: false,
-    autoplayTimeout: 4000,
-    autoplayHoverPause: false,
-    smartSpeed: 2000,
-    responsive: {
-      0: {
-        items: 2,
-      },
-      550: {
-        items: 3,
-      },
-      767: {
-        items: 3,
-      },
-      768: {
-        items: 4,
-      },
-      991: {
-        items: 5,
-      },
-      1000: {
-        items: 6,
-      },
-    },
-  });
+  // var owl = $(".brands-carousel");
+  // owl.owlCarousel({
+  //   items: 5,
+  //   loop: true,
+  //   autoplay: true,
+  //   navBy: 1,
+  //   nav: false,
+  //   autoplayTimeout: 4000,
+  //   autoplayHoverPause: false,
+  //   smartSpeed: 2000,
+  //   responsive: {
+  //     0: {
+  //       items: 2,
+  //     },
+  //     550: {
+  //       items: 3,
+  //     },
+  //     767: {
+  //       items: 3,
+  //     },
+  //     768: {
+  //       items: 4,
+  //     },
+  //     991: {
+  //       items: 5,
+  //     },
+  //     1000: {
+  //       items: 6,
+  //     },
+  //   },
+  // });
 
   /*----------------------------------------------------*/
   /*	Register Form Validation
 		/*----------------------------------------------------*/
 
-  $(".register-form").validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 2,
-        maxlength: 16,
-      },
-      email: {
-        required: true,
-        email: true,
-      },
-      phone: {
-        required: true,
-        digits: true,
-      },
-    },
-    messages: {
-      name: {
-        required: "Please enter no less than (1) characters",
-      },
-      email: {
-        required: "We need your email address to contact you",
-        email: "Your email address must be in the format of name@domain.com",
-      },
-      phone: {
-        required: "Please enter only digits",
-        digits: "Please enter a valid number",
-      },
-    },
-  });
+  // $(".register-form").validate({
+  //   rules: {
+  //     name: {
+  //       required: true,
+  //       minlength: 2,
+  //       maxlength: 16,
+  //     },
+  //     email: {
+  //       required: true,
+  //       email: true,
+  //     },
+  //     phone: {
+  //       required: true,
+  //       digits: true,
+  //     },
+  //   },
+  //   messages: {
+  //     name: {
+  //       required: "Please enter no less than (1) characters",
+  //     },
+  //     email: {
+  //       required: "We need your email address to contact you",
+  //       email: "Your email address must be in the format of name@domain.com",
+  //     },
+  //     phone: {
+  //       required: "Please enter only digits",
+  //       digits: "Please enter a valid number",
+  //     },
+  //   },
+  // });
 
   /*----------------------------------------------------*/
   /*	Contact Form Validation
 		/*----------------------------------------------------*/
 
-  $(".contact-form").validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 1,
-        maxlength: 16,
-      },
-      email: {
-        required: true,
-        email: true,
-      },
-      message: {
-        required: true,
-        minlength: 2,
-      },
-    },
-    messages: {
-      name: {
-        required: "Please enter no less than (1) characters",
-      },
-      email: {
-        required: "We need your email address to contact you",
-        email: "Your email address must be in the format of name@domain.com",
-      },
-      message: {
-        required: "Please enter no less than (2) characters",
-      },
-    },
-  });
+  // $(".contact-form").validate({
+  //   rules: {
+  //     name: {
+  //       required: true,
+  //       minlength: 1,
+  //       maxlength: 16,
+  //     },
+  //     email: {
+  //       required: true,
+  //       email: true,
+  //     },
+  //     message: {
+  //       required: true,
+  //       minlength: 2,
+  //     },
+  //   },
+  //   messages: {
+  //     name: {
+  //       required: "Please enter no less than (1) characters",
+  //     },
+  //     email: {
+  //       required: "We need your email address to contact you",
+  //       email: "Your email address must be in the format of name@domain.com",
+  //     },
+  //     message: {
+  //       required: "Please enter no less than (2) characters",
+  //     },
+  //   },
+  // });
 
   /*----------------------------------------------------*/
   /*	Comment Form Validation
 		/*----------------------------------------------------*/
 
-  $(".comment-form").validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 1,
-        maxlength: 16,
-      },
-      email: {
-        required: true,
-        email: true,
-      },
-      message: {
-        required: true,
-        minlength: 2,
-      },
-    },
-    messages: {
-      name: {
-        required: "Please enter no less than (1) characters",
-      },
-      email: {
-        required: "We need your email address to contact you",
-        email: "Your email address must be in the format of name@domain.com",
-      },
-      message: {
-        required: "Please enter no less than (2) characters",
-      },
-    },
-  });
+  // $(".comment-form").validate({
+  //   rules: {
+  //     name: {
+  //       required: true,
+  //       minlength: 1,
+  //       maxlength: 16,
+  //     },
+  //     email: {
+  //       required: true,
+  //       email: true,
+  //     },
+  //     message: {
+  //       required: true,
+  //       minlength: 2,
+  //     },
+  //   },
+  //   messages: {
+  //     name: {
+  //       required: "Please enter no less than (1) characters",
+  //     },
+  //     email: {
+  //       required: "We need your email address to contact you",
+  //       email: "Your email address must be in the format of name@domain.com",
+  //     },
+  //     message: {
+  //       required: "Please enter no less than (2) characters",
+  //     },
+  //   },
+  // });
 
   /*----------------------------------------------------*/
   /*	Newsletter Subscribe Form
 		/*----------------------------------------------------*/
 
-  $(".newsletter-form").ajaxChimp({
-    language: "cm",
-    url: "https://dsathemes.us3.list-manage.com/subscribe/post?u=af1a6c0b23340d7b339c085b4&id=344a494a6e",
-    //http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
-  });
+  // $(".newsletter-form").ajaxChimp({
+  //   language: "cm",
+  //   url: "https://dsathemes.us3.list-manage.com/subscribe/post?u=af1a6c0b23340d7b339c085b4&id=344a494a6e",
+  //   //http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+  // });
 
-  $.ajaxChimp.translations.cm = {
-    submit: "Submitting...",
-    0: "We have sent you a confirmation email",
-    1: "Please enter your email address",
-    2: "An email address must contain a single @",
-    3: "The domain portion of the email address is invalid (the portion after the @: )",
-    4: "The username portion of the email address is invalid (the portion before the @: )",
-    5: "This email address looks fake or invalid. Please enter a real email address",
-  };
+  // $.ajaxChimp.translations.cm = {
+  //   submit: "Submitting...",
+  //   0: "We have sent you a confirmation email",
+  //   1: "Please enter your email address",
+  //   2: "An email address must contain a single @",
+  //   3: "The domain portion of the email address is invalid (the portion after the @: )",
+  //   4: "The username portion of the email address is invalid (the portion before the @: )",
+  //   5: "This email address looks fake or invalid. Please enter a real email address",
+  // };
 });
